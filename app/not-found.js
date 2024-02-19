4536222524b13a6e8f6127458945e5529a48a808
mila2999 <mila.savonik@gmail.com>
2024-02-19 14:36:44 +0200

    Handeling not found state

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+'use client';
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <p>Not found</p>
+      <p>Unfortunately, we could not find the requested page or resourse.</p>
+    </main>
+  );
+}
