963cb08dc44860116429208e5cf6cc57304e68d9
mila2999 <mila.savonik@gmail.com>
2024-02-19 13:56:23 +0200

    Use Suspens for loading state

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,15 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
+
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lip/meals';
-import Link from 'next/link';
 import classes from './page.module.css';
-export default async function MealsPage() {
+
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +23,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
