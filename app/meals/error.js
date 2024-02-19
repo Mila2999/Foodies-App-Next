82adeaf676d1cd7ee3061bb2b46ac1233d912e07
mila2999 <mila.savonik@gmail.com>
2024-02-19 14:18:49 +0200

    Handelingg errors

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+'use client';
+export default function Error() {
+  return (
+    <main className="error">
+      <p>An error occured!</p>
+      <p>Failed to fetch meal data. Please try again later!</p>
+    </main>
+  );
+}
