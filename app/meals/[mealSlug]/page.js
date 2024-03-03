9bf7b3739e38bcfa7b9c6d0faf27a10a2adecec8
mila2999 <mila.savonik@gmail.com>
2024-03-03 10:34:42 +0200

    Added dinamyc meta data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,6 +2,15 @@ import { notFound } from 'next/navigation';
 import { getMeal } from '@/lip/meals';
 import Image from 'next/image';
 import classes from './page.module.css';
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return { title: meal.title, description: meal.summary };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
   if (!meal) {
