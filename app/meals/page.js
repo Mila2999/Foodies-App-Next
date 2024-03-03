307f9a19d079f35158213b3bcf01d4f7586c8320
mila2999 <mila.savonik@gmail.com>
2024-03-03 10:23:22 +0200

    Added static meta data

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lip/meals';
 import classes from './page.module.css';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
